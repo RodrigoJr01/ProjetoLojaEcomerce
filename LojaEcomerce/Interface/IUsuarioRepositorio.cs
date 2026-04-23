49dfaa57853f872f2fa7c5ae59daefce88539439
Rodrigo Jr <zinxrodrigo01@gmail.com>
2026-04-23 16:46:58 -0300

    feat: codificando repositorio e interface

---
+++ LojaEcomerce/Interface/IUsuarioRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using LojaEcomerce.Models;
+
+namespace LojaEcomerce.Interface
+{
+    public interface IUsuarioRepositorio
+    {
+        // A interface não contem codigo apenas a promessa do que se deve ser feito(como um contrato)
+        LoginViewModel Validar(string email, string senha);
+    }
+}
---
+++ LojaEcomerce/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,44 @@
+﻿using LojaEcomerce.Models;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Cms;
+
+namespace LojaEcomerce.Repositorio
+{
+    public class UsuarioRepositorio
+    {
+        //variavel privada e somenete para leitura
+        private readonly string _connectionString;
+
+        public UsuarioRepositorio(IConfiguration config)
+        {
+            _connectionString = config.GetConnectionString("Conexão");
+        }
+        //metodo para validar o login 
+
+
+        public LoginViewModel Validar(string email, string senha)
+        {
+            using var conn = new MySqlConnection(_connectionString);
+            conn.Open();
+
+            var sql = "SELECT * FROM tb_Usuario WHERE Email= @email AND Senha = @senha";
+            var cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@email", email);
+            cmd.Parameters.AddWithValue("@senha", senha);
+
+            using var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                return new LoginViewModel
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Nome = reader["Nome"].ToString(),
+                    Email = reader["Email"].ToString(),
+                    Nivel = reader["Nivel"].ToString()
+                };
+            }
+            return null;
+
+        }
+    }
+}
